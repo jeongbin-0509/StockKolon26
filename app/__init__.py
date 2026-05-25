edce507f3d1be09726f4a71aca923371b36f7e1f
BaboJayden <smvictory77@gmail.com>
2026-05-26 01:27:42 +0900

    structure construction

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+# Flask 클래스 가져오기
+from flask import Flask
+
+
+# Flask 앱 생성 함수
+def create_app():
+
+    # Flask 서버 객체 생성
+    app = Flask(__name__)
+
+    # routes/main.py 에서 main Blueprint 가져오기
+    from app.routes.main import main
+
+    # Blueprint 등록
+    app.register_blueprint(main)
+
+    # 완성된 Flask 앱 반환
+    return app
